07912a0a7d6499975dc78d9e7db072d314709d3b
pablodecm <pablodecm@gmail.com>
2018-02-20 16:28:24 +0000

    use Dataset API and simplify code

--- code/toy_1D_asimov_train.py
+++ code/toy_1D_asimov_train.py
@@ -20,6 +20,7 @@ tf.flags.DEFINE_integer("steps", default=1,
 tf.flags.DEFINE_integer("n_train_eval", default=1,
                        help="number of steps for each train call")
 tf.flags.DEFINE_integer("batch_size", default=4000, help="")
+tf.flags.DEFINE_float("temperature", default=1.0, help="")
 
 
 flags = tf.flags.FLAGS
@@ -44,43 +45,56 @@ def asimov_model(features, labels, mode, params):
 
     layers = small_nn()
 
-    inputs = {}
-    logits = {}
-    probs = {}
-    input_keys =  ["sig", "bkg"]
+    temperature = tf.convert_to_tensor(params["temperature"], dtype=tf.float32) 
+    c_interest  = params["c_interest"] 
 
-    for key in input_keys:
-    
-      net = tf.reshape(features[key], (-1,1))
-      for layer in layers.values():
-        net = layer(net) 
-      logits[key] = net  
+    if "components" in features:
+      # get name of each component
+      c_names = sorted(features["components"].keys())
+      c_tensors = [features["components"][c_name] for c_name in c_names] 
+      c_sizes = [tf.shape(c_tensor)[0] for c_tensor in c_tensors]
+      if "c_norms" in features:
+        c_norms = [features["c_norms"][c_name]  for c_name in c_names]
+      for c_name, c_size in zip(c_names, c_sizes):
+        tf.summary.scalar("c_batch_size_{}".format(c_name), c_size)
+      X = tf.concat(c_tensors, axis=0, name="concat_components")
+      y = tf.concat([tf.ones((c_size,),dtype=tf.int32)*i 
+        for i, c_size in enumerate(c_sizes)], axis=0) 
+    else:
+      X = features["X"]
+      y = labels 
 
-      probs[key] = tf.nn.softmax(logits[key])
+    inputs = tf.reshape(X, (-1,1))
 
+    net = inputs
+    for layer in layers.values():
+      net = layer(net) 
+    logits = net  
+
+    probs = tf.nn.softmax(logits/temperature)
 
     if mode == tf.estimator.ModeKeys.PREDICT:
-      predictions = probs
+      predictions = {"probabilities" : probs}
       return tf.estimator.EstimatorSpec(mode, predictions=predictions)
 
-    means = { key : tf.reduce_mean(probs[key], axis=0) for key in input_keys}
-    exp_sig = tf.convert_to_tensor(20., name="exp_bkg")
-    exp_bkg = tf.convert_to_tensor(400., name="exp_sig")
-    mu = tf.convert_to_tensor(1., name="s_exp")
-    norms = { "sig" : exp_sig*mu,
-              "bkg" : exp_bkg }
-       
-    exp_counts = sum([means[key]*norms[key] for key in input_keys]) 
+    split_probs = tf.split(probs, c_sizes, name="split_probs")
+    split_means = [tf.reduce_mean(split_prob, axis=0) for split_prob in split_probs]
 
+    mu = tf.convert_to_tensor(1., name="mu")
+    split_counts = [ mean*norm*mu if (c_name==c_interest) else mean*norm
+        for c_name, mean, norm in zip(c_names, split_means, c_norms)]
+       
+    exp_counts = sum(split_counts) 
+
+    # soft-loss
     pois = ds.Poisson(exp_counts[1])
     nll = - pois.log_prob(tf.stop_gradient(exp_counts[1]))
-
     hess = batch_hessian(nll, [mu])
-
     loss = tf.reduce_sum(tf.sqrt(1./hess[0][0]))
 
     if mode == tf.estimator.ModeKeys.EVAL:
-      return tf.estimator.EstimatorSpec( mode, loss=loss)
+
+      return tf.estimator.EstimatorSpec( mode, loss=loss )
 
     assert mode == tf.estimator.ModeKeys.TRAIN
 
@@ -93,26 +107,40 @@ def main(_):
 
   data = np.load(flags.train_data)
 
-  clf = tf.estimator.Estimator(model_fn=asimov_model, model_dir=flags.model_dir)
+  params = { "temperature" : flags.temperature,
+             "c_interest" : "sig"}
 
-  train_sig, val_sig, train_bkg, val_bkg = train_test_split(data["sig"], data["bkg"])
+  clf = tf.estimator.Estimator(model_fn=asimov_model, model_dir=flags.model_dir,
+                               params=params)
 
-  train_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={"sig" : train_sig, "bkg" : train_bkg},
-      y=np.ones(train_sig.shape[0]),
-      num_epochs=None,
-      batch_size=flags.batch_size,
-      shuffle=True)
+  train_data = {}
+  val_data = {}
+  for key, value in data.items():
+    train_data[key], val_data[key] = train_test_split(value) 
 
-  val_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={"sig" : val_sig, "bkg" : val_bkg },
-      y=np.ones(val_sig.shape[0]),
-      batch_size=flags.batch_size,
-      shuffle=False)
+  print(data.keys())
+
+  def train_input_fn():
+
+    components = {}
+    for key, value in train_data.items():
+      components[key] = tf.data.Dataset.from_tensor_slices(value)\
+                             .shuffle(buffer_size=10000)\
+                             .batch(flags.batch_size)
+
+    c_norms = tf.data.Dataset.from_tensors({"sig" : 20., "bkg" : 400.}).repeat()
+
+    dataset = tf.data.Dataset.zip({"components" : components, "c_norms" : c_norms})
+
+    dataset_it = dataset.make_one_shot_iterator()
+    next_batch = dataset_it.get_next()
+
+    return next_batch, None 
+
 
   for i in range(flags.n_train_eval):
     clf.train(input_fn=train_input_fn, steps=flags.steps)
-    clf.evaluate(input_fn=val_input_fn)
+    clf.evaluate(input_fn=train_input_fn)
 
 if __name__ == "__main__":
   tf.app.run()
